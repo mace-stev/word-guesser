296e86990330c0b58b42f720612117fe71faff18
Mason Stevenson <williamstevenson107@gmail.com>
2024-02-08 20:42:52 -0600

    decided to just use two apis so that the page is more likely to have some kind of info rendered and the user doesn't have to default to a google search

--- backend/controllers/dictionaryController.js
+++ backend/controllers/dictionaryController.js
@@ -3,30 +3,42 @@ const axios = require('axios')
 
 
 
-exports.dictionary= async (req, res)=>{
-    const result={};
-    const word=req.body.word
-    const headers = {
-        'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
-        'X-RapidAPI-Host': process.env.RAPIDAPI_HOST
-      };
+exports.dictionary = async (req, res) => {
+  const result = {
+    meanings: {
+      examples: []
+    }
+  };
+  const word = req.body.word
+  const headers = {
+    'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
+    'X-RapidAPI-Host': process.env.RAPIDAPI_HOST
+  };
+
+  const params = {
+    entry: word
+  };
+  await axios.get(`https://twinword-word-graph-dictionary.p.rapidapi.com/definition/`, {
+    headers,
+    params
+  })
+    .then((response) => {
+      result['meanings'] = response.data.meaning;
+    }).catch((error) => { console.log("error getting meaning from twinword api") })
+  await axios.get(`https://twinword-word-graph-dictionary.p.rapidapi.com/example/`, {
+    headers,
+    params
+  }).then((response) => {
+  
+    if (response.data.example) {
+      result['meanings']['examples'] = response.data.example
+    }
+  }).catch((console.log("error getting examples from twinword api")))
+  await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then((response) => {
       
-      const params = {
-        entry: word
-      };
-    await axios.get(`https://twinword-word-graph-dictionary.p.rapidapi.com/definition/`,{
-        headers,
-        params
-      }) 
-    .then((response)=>{
-         result['meanings']=response.data.meaning;
-         axios.get(`https://twinword-word-graph-dictionary.p.rapidapi.com/example/`,{
-            headers,
-            params
-          }).then((response)=>{
-            console.log(response)
-            result['meanings']['examples']=response.data.example
-            res.status(201).send(result)
-          }) 
-        }).catch((error)=>{console.log(error)})
+      result['definitions'] = response.data[0]['meanings'][0]['definitions']
+    })
+    .catch((error) => { console.log("error getting data from dictionary api") })
+  res.status(201).send(result)
 }
--- frontend/src/components/WordDefinitions/WordDefinitions.js
+++ frontend/src/components/WordDefinitions/WordDefinitions.js
@@ -8,8 +8,8 @@ function WordDefinitions(){
     const [definition, setDefinition]= useState()
     useEffect(()=>{
     axios.post("/words/definition",{word: word}).then((response)=>{
-        setDefinition(response.data.meanings)
-        console.log(response.data.meanings)
+        setDefinition(response.data)
+
     }).catch((error)=>{console.log("error getting word definition")})
 },[word])
     return(
@@ -18,14 +18,21 @@ function WordDefinitions(){
     <h1>{`${word.toUpperCase()}`}</h1>
     <a href={`https://www.google.com/search?q=${word}`} className="backup-link" target="_blank" rel="noopener noreferrer">If nothing is rendered, you can continue your search here.</a>
         <div className="word-definitions__container">
-        <p><span>Noun Meaning:</span> {`${definition?.noun || ""}`}</p>
-        <p><span>Verb Meaning:</span> {`${definition?.verb || ""}`}</p>
-        <p><span>Adjective Meaning:</span> {`${definition?.adjective || ""}`}</p>
-        <p><span>Adverb Meaning:</span> {`${definition?.adverb || ""}`}</p>
-        <p><span>Example 1:</span> {`${definition?.examples[0] || ""}`}</p>
-        <p><span>Example 2:</span> {`${definition?.examples[1] || ""}`}</p>
-
+        <p><span>Noun Meaning:</span> {`${definition?.meanings?.noun || ""}`}</p>
+        <p><span>Verb Meaning:</span> {`${definition?.meanings?.verb || ""}`}</p>
+        <p><span>Adjective Meaning:</span> {`${definition?.meanings?.adjective || ""}`}</p>
+        <p><span>Adverb Meaning:</span> {`${definition?.meanings?.adverb || ""}`}</p>
+        <p><span>Example 1:</span> {`${definition?.meanings?.examples[0] || ""}`}</p>
+        <p><span>Example 2:</span> {`${definition?.meanings?.examples[1] || ""}`}</p>
         </div>
+        {definition?.definitions?.map((element, index)=>{
+        return(<div key={index} className="word-definitions__container">
+        <p><span>Definition:</span> {`${element?.definition || ""}`}</p>
+        <p><span>Synonym:</span> {`${element?.synonyms[0] || ""}`}</p>
+        <p><span>Antonym:</span> {`${element?.antonyms[0] || ""}`}</p>
+        <p><span>Example:</span> {`${element?.example || ""}`}</p>
+        </div>)
+    })}
     
     </section>)
 }
